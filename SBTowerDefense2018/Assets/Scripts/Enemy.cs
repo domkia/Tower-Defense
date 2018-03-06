27caf32efa6ab87008f0925d5078c7a5b51beed2
Domantas <domkia@users.noreply.github.com>
2018-03-06 17:18:53 +0200

    Enemy base class

---
+++ SBTowerDefense2018/Assets/Scripts/Enemy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+/// <summary>
+/// Base Enemy class
+/// </summary>
+abstract class Enemy
+{
+    public abstract event Action OnEnemyDied;
+    public abstract void TakeDamage();
+}
