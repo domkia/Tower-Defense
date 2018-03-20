d38bec3eca6ff918e15a012dff68fb08d2b1caf3
rokastve <36890057+rokastve@users.noreply.github.com>
2018-03-20 16:49:00 +0200

    Pause menu
    Pauses game, nothing moves, can't interact with anything on the map

---
+++ SBTowerDefense2018/Assets/Pause.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    [SerializeField] private GameObject pausePanel;
+
+    bool paused;
+    void Start()
+    {
+        pausePanel.SetActive(false);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (paused)
+            {
+                Time.timeScale = 0;
+                pausePanel.SetActive(true);
+            }
+            else
+            {
+                Time.timeScale = 1;
+                pausePanel.SetActive(false);
+            }
+            paused = !paused;
+        }
+    }
+    private void PauseGame()
+    {
+        if (paused)
+        {
+            Time.timeScale = 0;
+            pausePanel.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pausePanel.SetActive(false);
+        }
+        paused = !paused;
+    }
+}
