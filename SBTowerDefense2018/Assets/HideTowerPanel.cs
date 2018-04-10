1f9348dd8314ca552b4e82af8978c45e354ee4ad
rokastve <36890057+rokastve@users.noreply.github.com>
2018-04-10 12:26:53 +0300

    Missing files add

---
+++ SBTowerDefense2018/Assets/HideTowerPanel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideTowerPanel : MonoBehaviour {
+
+    public GameObject towerPanel;
+
+    private void Start()
+    {
+        towerPanel.SetActive(false);
+    }
+    public void hidePanel()
+    {
+        towerPanel.SetActive(false);
+    }
+    public void showPanel()
+    {
+        towerPanel.SetActive(true);
+    }
+
+}
