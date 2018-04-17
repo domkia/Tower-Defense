be7ed79e12f6977447c3c12f17c9ddc7aadcd3c8
Aldas Taraškevičius <aldas.taraskevicius@ktu.edu>
2018-04-17 13:26:45 +0300

    Reorganized project folder

--- SBTowerDefense2018/Assets/IDamagable.cs
+++
@@ -1,7 +0,0 @@
-﻿
-interface IDamagable<T>
-{
-    int Health { get; set; }
-    void TakeDamage(int damage);
-    event System.Action<T> OnDeath;
-}
---
+++ SBTowerDefense2018/Assets/Scripts/Interfaces/IDamagable.cs
@@ -0,0 +1,7 @@
+﻿
+interface IDamagable<T>
+{
+    int Health { get; set; }
+    void TakeDamage(int damage);
+    event System.Action<T> OnDeath;
+}
