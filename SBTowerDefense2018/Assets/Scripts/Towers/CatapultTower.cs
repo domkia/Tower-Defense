bc2bcb0b8c33622d8ef341a5f3775dfdbacef2b3
Domantas Kiaušinis <domantas.kiausinis@ktu.edu>
2018-04-24 15:04:10 +0300

    Catapult animations etc.
    - WIP catapult tower

---
+++ SBTowerDefense2018/Assets/Scripts/Towers/CatapultTower.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CatapultTower : AOETower
+{
+    public Animator animator;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    protected override void SetupTilesInRange()
+    {
+        TilesInRange = HexGrid.Instance.GetTilesRing(BuiltOn, Range);
+    }
+
+    public override void Attack()
+    {
+        base.Attack();
+        animator.SetTrigger("Shoot");
+    }
+}
