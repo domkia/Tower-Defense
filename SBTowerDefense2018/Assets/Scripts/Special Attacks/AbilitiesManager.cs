ad6aef622d4db959d83101f70009db601e2e1163
Belenkox <justinas.slabada@ktu.edu>
2018-04-24 13:08:39 +0300

    Special attacks base class
    special attacks base class!

---
+++ SBTowerDefense2018/Assets/Scripts/Special Attacks/AbilitiesManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilitiesManager : MonoBehaviour {
+
+    private List<SpecialAttack> specialAttack = new List<SpecialAttack>(3);
+    
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (SpecialAttack SA in specialAttack)
+        {
+            SA.UpdateCooldown();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            specialAttack[0].Do();
+        }
+	}
+}
---
+++ SBTowerDefense2018/Assets/Scripts/Special Attacks/IncreaseFireRate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncreaseFireRate : SpecialAttack
+{
+    public override float cooldown
+    {
+        get
+        {
+            return 60;
+        }
+    }
+
+    public override void Do()
+    {
+        timer = cooldown;
+        isReady = false;
+    }
+}
