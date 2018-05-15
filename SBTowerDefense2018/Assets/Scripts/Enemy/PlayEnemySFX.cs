503415d7ce166f3f82e8c82231d47c9e84d6577f
rokastve <36890057+rokastve@users.noreply.github.com>
2018-05-15 16:14:22 +0300

    Added sounds to enemies
    - Added enemies getting shot sound

--- SBTowerDefense2018/Assets/Scripts/Enemy/PlayEnemySFX.cs
+++ SBTowerDefense2018/Assets/Scripts/Enemy/PlayEnemySFX.cs
@@ -6,9 +6,6 @@
 [RequireComponent(typeof(AudioSource))]
 public class PlayEnemySFX : MonoBehaviour
 {
-    // Sound effects played when an enemy gets hit by a projectile.
-    // These sound effects play every single time.
-    public AudioClip[] EnemyHitSFX;
     // Sound effects played when an enemy is hurt but not killed.
     // These sound effects don't play every single time. Instead,
     // they play based on random chance.
@@ -24,21 +21,19 @@ public class PlayEnemySFX : MonoBehaviour
     private AudioSource source;
 
     // Volumes are slightly randomized.
-    private float volLoBound = 0.5f;
-    private float volHiBound = 1.0f;
+    private float volLoBound = 0.2f;
+    private float volHiBound = 0.5f;
 
     public void Play(SoundType type)
     {
-        // First, choose a random hit sound effect and play it.
-        int hitSFXindex = Random.Range(0, EnemyHitSFX.Length);
         float vol = Random.Range(volLoBound, volHiBound);
-        source.PlayOneShot(EnemyHitSFX[hitSFXindex], vol);
 
         if(type == SoundType.EnemyPain)
         {
             bool soundWillPlay = Random.Range(0.0f, 1.0f) < PainChance;
             if(soundWillPlay)
             {
+                source.pitch = Random.Range(0.7f, 1.3f);
                 int painSFXindex = Random.Range(0, EnemyPainSFX.Length);
                 source.PlayOneShot(EnemyPainSFX[painSFXindex], vol);
             }
