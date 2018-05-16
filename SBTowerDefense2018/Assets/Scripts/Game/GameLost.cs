1d7fb741b3977014de725c0499bb5fa55025bf94
Valdas Karčauskas <valdas.karcauskas@ktu.edu>
2018-05-16 14:30:57 +0300

    UIN21 When game lost screen is fading

--- SBTowerDefense2018/Assets/Scripts/Game/GameLost.cs
+++ SBTowerDefense2018/Assets/Scripts/Game/GameLost.cs
@@ -1,20 +1,31 @@
 ﻿using UnityEngine;
-
+using UnityEngine.SceneManagement;
+using DG.Tweening;
 public class GameLost : MonoBehaviour
 {
     public GameObject gameOverPanel;
+    
 
     void Start()
     {
         gameOverPanel.SetActive(false);
         GameManager.OnGameOver += EndGame;
+        gameOverPanel.GetComponent<CanvasGroup>().alpha = 0;
     }
 
     void EndGame()
     {
         GameManager.OnGameOver -= EndGame;
         Debug.Log("GAME OVER!");
+        
+        //gameOverPanel.SetActive(true);
         gameOverPanel.SetActive(true);
-        Time.timeScale = 0;
+        gameOverPanel.GetComponent<CanvasGroup>().DOFade(1, 1).OnComplete(() => Showgameover());
+       // Time.timeScale = 0;
+
+    }
+    void Showgameover()
+    {
+        gameOverPanel.GetComponent<CanvasGroup>().interactable = true;
     }
 }
