f79642f59b4d716a958b92e2c541096709a01134
rokastve <36890057+rokastve@users.noreply.github.com>
2018-05-22 14:01:27 +0300

    Demo Rotate Scene
    - Added scene to create trailer

---
+++ SBTowerDefense2018/Assets/Scripts/CameraClass.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class CameraClass : MonoBehaviour
+{
+
+    public GameObject target;//the target object
+    private float speedMod = 10.0f;//a speed modifier
+    private Vector3 point;//the coord to the point where the camera looks at
+
+    void Start()
+    {//Set up things on the start method
+        point = target.transform.position;//get target's coords
+        transform.LookAt(point);//makes the camera look to it
+    }
+
+    void Update()
+    {//makes the camera rotate around "point" coords, rotating around its Y axis, 20 degrees per second times the speed modifier
+        transform.RotateAround(target.transform.position, Vector3.up, speedMod * Time.deltaTime);
+    }
+}
